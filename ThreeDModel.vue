542d4be8412b0bbbcc202762faecf8b87551bd81
nyfeldt-geins <nyfeldt@geins.com>
2023-03-31 15:21:12 +0200

    add: environment to scene

--- ThreeDModel.vue
+++ ThreeDModel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div @click="handleClick" ref="wrapper" id="model-wrapper">
     <canvas id="model"></canvas>
   </div>
 </template>
@@ -9,106 +9,68 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import SceneInit from "./lib/SceneInit";
 
 export default {
   name: "ThreeDModel",
+  ssr: false,
   props: {
-    // fileName: {
-    //   type: String,
-    //   required: true,
-    // },
-    // fileType: {
-    //   type: String,
-    //   required: true,
-    //   validator(value) {
-    //     return ["gltf", "glb"].includes(value);
-    //   },
-    // },
-  },
-  data: () => ({
-    width: 200,
-    height: 200,
-  }),
-  computed: {
-    // filePath() {
-    //   return this.fileType === "gltf"
-    //     ? `/3d-models/${this.fileName}/scene.${this.fileType}`
-    //     : `/3d-models/${this.fileName}.glb`;
-    // },
-    filePath() {
-      return "/3d-models/nike/scene.gltf";
+    filePath: {
+      type: String,
+      required: true,
+    },
+    rotate: {
+      type: Boolean,
+      default: false,
     },
   },
+  data: () => ({
+    scene: undefined,
+    camera: undefined,
+    renderer: undefined,
+    fov: 60,
+    nearPlane: 1,
+    farPlane: 1000,
+    canvasId: "model",
+    wrapperId: "model-wrapper",
+    clock: undefined,
+    orbitControls: undefined,
+    ambientLight: undefined,
+    directionalLight: undefined,
+    loadedGltf: undefined,
+  }),
+  computed: {},
+  watch: {},
   mounted() {
     if (this.filePath) {
-      console.log("log-- modelPath", this.modelPath);
-      const model = new SceneInit("model");
-      console.log("log-- model", model);
-      model.initialize(this.width, this.height);
-      model.animate();
-
-      let loadedModel;
+      const scene = new SceneInit(this.canvasId, this.wrapperId);
       const gltfLoader = new GLTFLoader();
       const dracoLoader = new DRACOLoader();
+
       gltfLoader.setCrossOrigin("anonymous");
       dracoLoader.setDecoderPath("/examples/jsm/libs/draco/");
       gltfLoader.setDRACOLoader(dracoLoader);
-
       gltfLoader.load(
         `${this.filePath}`,
-        (gltfScene) => {
-          loadedModel = gltfScene;
-          console.log("log-- loadedModel", loadedModel);
-          const { scene } = gltfScene;
-          const box = new THREE.Box3().setFromObject(scene);
+        (gltf) => {
+          // loadedModel = gltf;
+          this.gltfLoaded = true;
+          this.loadedModel = gltf;
+          console.log("log-- gltf", gltf);
+          const gltfScene = gltf.scene;
+          // const { scene } = gltf;
+          const box = new THREE.Box3().setFromObject(gltfScene);
           const size = box.getSize(new THREE.Vector3()).length();
           const center = box.getCenter(new THREE.Vector3());
-          console.log("log-- box", box);
-          console.log("log-- size", size);
-          console.log("log-- center", center);
-          // scene.position.x += scene.position.x - center.x;
-          // scene.position.y += scene.position.y - center.y;
-          // scene.position.z += scene.position.z - center.z;
-          model.setControls(size);
-          model.setCamera(size, center);
-          // this.controls.maxDistance = size * 10;
-          // this.defaultCamera.near = size / 100;
-          // this.defaultCamera.far = size * 100;
-          // this.defaultCamera.updateProjectionMatrix();
 
-          // if (this.options.cameraPosition) {
-          //   this.defaultCamera.position.fromArray(this.options.cameraPosition);
-          //   this.defaultCamera.lookAt(new Vector3());
-          // } else {
-          //   this.defaultCamera.position.copy(center);
-          //   this.defaultCamera.position.x += size / 2.0;
-          //   this.defaultCamera.position.y += size / 5.0;
-          //   this.defaultCamera.position.z += size / 2.0;
-          //   this.defaultCamera.lookAt(center);
-          // }
-
-          // var box = new THREE.Box3().setFromObject(scene);
-          // var center = new THREE.Vector3();
-          // box.getCenter(center);
-          // scene.position.sub(center); // center the model
-          // console.log("log-- loadedModel", loadedModel);
-          // console.log("log-- animations", loadedModel.animations);
-          // console.log("log-- asset", loadedModel.asset);
-          // console.log("log-- cameras", loadedModel.cameras);
-          // console.log("log-- parser", loadedModel.parser);
-          console.log("log-- scene", loadedModel.scene);
-          console.log("log-- location", location);
-          // console.log("log-- scenes", loadedModel.scenes);
-          // console.log("log-- userData", loadedModel.userData);
-
-          // loadedModel.scene.rotation.y = Math.PI / 8;
-          // loadedModel.scene.position.set(0, 0, 0);
-          // loadedModel.scene.up.set(0, 0, 0);
-          // loadedModel.scene.position.y = 1;
-          // loadedModel.scene.scale.set(5, 5, 5);
-
-          model.scene.add(scene);
+          scene.initialize(400, 300);
+          scene.animate();
+          scene.setControls(size);
+          scene.setCamera(size, center);
+          scene.scene.add(gltfScene);
+          scene.animate();
+          this.rotate && scene.rotate();
         },
         (xhr) => {
           console.log("log-- ", xhr);
@@ -117,24 +79,18 @@ export default {
           console.log("log-- an error occured", error);
         }
       );
-
-      const animate = () => {
-        if (loadedModel) {
-          // loadedModel.scene.rotation.x += 0.01;
-          // loadedModel.scene.rotation.y += 0.01;
-          // loadedModel.scene.rotation.z += 0.01;
-        }
-        requestAnimationFrame(animate);
-      };
-      animate();
     }
   },
-  methods: {},
+  methods: {
+    handleClick() {
+      this.$emit("clicked", "hej från 3d model viewer");
+    },
+  },
 };
 </script>
 
-<style>
-.wrapper {
-  background-color: blue;
+<style scoped>
+#model {
+  cursor: grab;
 }
 </style>
